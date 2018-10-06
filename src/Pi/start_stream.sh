00a7900eee4e32ae480a765e2d68b1f4675868bc
Muhammed Karlitepe <m.karlitepe@hotmail.com>
2018-10-06 08:12:36 +0100

    minior changes

--- src/Pi/start_stream.sh
+++ src/Pi/start_stream.sh
@@ -1,5 +1,5 @@
 #cd mjpg
-mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -quality 30 --fps 15 -vf" &
+mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 640 -y 480 -quality 30 --fps 15 -vf -rot 180" &
 #..cd 
 #python3 robofriend.py
 
