27e703b07de26331e8a8e4d248384c7fd16415ea
momokarl <m.karlitepe@hotmail.com>
2018-10-31 17:10:54 +0100

    function to light the ears in different colors according to the face finished and debug prints added

--- src/Pi/python/facedetectionModule.py
+++ src/Pi/python/facedetectionModule.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 import teensyCommunicator
+import ioWarriorModule
 from facedetection_coordinates.msg import Coordinates
 
 # TODO: according to the known face let the ears light in different colors
@@ -17,6 +18,7 @@ def callback (data):
 
     centre_face(coordin)
     resize_face(coordin)
+    identify_face(coordin)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
@@ -24,29 +26,34 @@ def listener():
 
     rospy.spin()
 
-def known_face(coordinates):
+def identify_face(coordinates):
     if data["name"] != "Unknown":
-        return
-
-
-
+        ioWarriorModule.sendToIOWarrior(0, 15, 0) #if it's a known face let the ears light in green
+    else:
+        ioWarriorModule.sendToIOWarrior(15, 0, 0) #for a unknown face let the ears light in red
 
 def centre_face(coordinates):
     if (coordinates["x"] < 160):
         #move left
+        print("[INFO] x: {}, therefore move left".format(coordinates["x"]))
         teensyCommunicator.moveLeftStep()
     elif(coordinates["x"] > 160):
         #move right
+        print("[INFO] x: {}, therefore move right".format(coordinates["x"]))
         teensyCommunicator.moveRightStep()
     else:
+        print("[INFO] Face is in the centre")
         return
 
 def resize_face(coordinates):
     if (coordinates["x_w"] - coordinates["x"] < 100):   # TODO: check if parameters for ForwardStep and BackwardStep are ok
         # move forward
+        print("[INFO] Move forward / diff of x: {}".format(coordinates["x_w"] - coordinates["x"]))
         teensyCommunicator.moveForwardStep()
     elif(coordinates["x_w"] - coordinates["x"] > 160):
         # move backward
+        print("[INFO] Move backward / diff of x: {}".format(coordinates["x_w"] - coordinates["x"]))
         teensyCommunicator.moveBackStep()
     else:
+        print("[INFO] In the right distance!")
         return
