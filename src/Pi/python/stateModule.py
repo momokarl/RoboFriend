a8bf86f5a409c0a7da8149f036b8deb651c9f5dd
momokarl <m.karlitepe@hotmail.com>
2018-11-08 17:32:05 +0100

    Thread to handle the states added

--- src/Pi/python/stateModule.py
+++ src/Pi/python/stateModule.py
@@ -1,13 +1,29 @@
 #external modules
 import threading
+import time
+from transitions import Machine
+
+class Robo():
+    pass
 
 runFlag = True
 
 def start():
     print("[INFO] Starting state initialization")
-    threadHandler = threading.Thread(target = stateHanlder)
+    threadHandler = threading.Thread(target = state_handler)
     threadHandler.daemon = True
     threadHandler.start()
 
-def stateHandler():
+def state_handler():
     print("[INFO] Within state handler thread ...")
+    state_config()
+
+def state_config():
+    states = ['init', 'idle', 'keyboard', 'face_audio_detection',
+              'gui_communicator', 'webserver', 'low_battery']
+    robostate = Robo()
+    stat = Machine(model = robostate, states = states, initial = 'init')
+    while runFlag:
+        #print(robostate.state)
+        print("[INFO] We are in the state: {}".format(robostate.state))
+        time.sleep(2)
