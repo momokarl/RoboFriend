2f604e4a6a3cc3b06caee97322722fef90775d6a
momokarl <m.karlitepe@hotmail.com>
2018-11-09 11:16:15 +0100

    start of each module is implemented and started when transaction from init state to idle state is triggered

--- src/Pi/python/stateModule.py
+++ src/Pi/python/stateModule.py
@@ -3,22 +3,41 @@ import threading
 import time
 from transitions import Machine
 
+#own modules
+import faceModule as faceModule
+import rfidModule as rfidModule
+import webserverModule as webserverModule
+import statusModule as statusModule
+import gameCommunicator as gameCommunicator
+import keyboardModule as keyboardModule
+import teensyCommunicator as teensyCommunicator
+import ioWarriorModule as ioWarriorModule
+import speechModule as speechModule
+import facedetectionModule as facedetectionModule
+
 class Robo(Machine):
     def __init__(self):
         states = [
                 'init', 'idle', 'keyboard',
                 'face_audio_detection','gui_communicator',
                 'webserver', 'low_battery']
-        Machine.__init__(self, states = states, initial = 'init')
-        #self.add_transition(trigger : 'leave_init', source : 'init', dest : 'idle', before = 'say_goodbye')
-        self.add_transition('leave_init', 'init', 'idle',
-                         before='say_goodbye',after = 'say_hello')
 
-    def say_goodbye(self):
-        print("[INFO] Goodbye init state")
+        Machine.__init__(self, states = states, initial = 'init')
+
+        self.add_transition(trigger = 'leave_init', source = 'init', dest = 'idle',
+                         before = 'start_modules', after = 'say_hello')
+
+    def start_modules(self):
+        rfidModule.start()
+        webserverModule.start()
+        statusModule.start()
+        gameCommunicator.start()
+        keyboardModule.start()
+        faceModule.drawFace()
+        print("[INFO] Leaving init state")
 
     def say_hello(self):
-        print("[INFO] Hello idle state")
+        print("[INFO] entering idle state")
 
 runFlag = True
 
@@ -29,14 +48,9 @@ def start():
     threadHandler.start()
 
 def state_handler():
-    print("[INFO] Within state handler thread ...")
     robostate = Robo()
-    #robostate.leave_init()
-    print("[INFO] Actual state {}".format(robostate.state))
-    robostate.leave_init()
-    print("[INFO] Actual state {}".format(robostate.state))
+    robostate.leave_init()  # to leave init state and enter idle state
 
-
-    # while runFlag:
-    #     print("[INFO] We are in the state: {}".format(robostate.state))
-    #     time.sleep(2)
+    while runFlag:
+         #print("[INFO] We are in the state: {}".format(robostate.state))
+         time.sleep(2)
